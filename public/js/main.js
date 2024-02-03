cc0e57d0ffae3ba6af61d27183319333ce81574b
WIZI <loick.konieczny@gmail.com>
2024-02-03 02:44:36 +0100

    optimize counter

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,72 @@ window.addEventListener("load", () => {
   document.getElementById("currentYear").innerHTML = new Date().getFullYear();
 })
 
+// =================
+// Counter animation
+// =================
 
+fetch(`/stats`)
+  .then(response => response.json())
+  .then(data => {
+    const createCounterElement = (icon, value, label) => {
+      const counterColumn = document.createElement("div");
+      counterColumn.className = "counter-flex__column";
+
+      const valueElement = document.createElement("div");
+      valueElement.className = "value";
+      valueElement.setAttribute("data-from", "0");
+      valueElement.setAttribute("data-to", value);
+      valueElement.textContent = value;
+
+      const iconElement = document.createElement("i");
+      iconElement.className = icon;
+
+      const labelElement = document.createElement("h2");
+      labelElement.appendChild(iconElement);
+      labelElement.appendChild(document.createTextNode(` ${label}`));
+
+      counterColumn.appendChild(valueElement);
+      counterColumn.appendChild(labelElement);
+
+      return counterColumn;
+    };
+
+    const counters = document.querySelector(".counter-flex");
+
+    counters.appendChild(createCounterElement("fas fa-server", data.server, "Servers"));
+    counters.appendChild(createCounterElement("fas fa-terminal", data.commands, "Commands"));
+    counters.appendChild(createCounterElement("fas fa-users", data.members, "Users"));
+
+    document.querySelectorAll('.value').forEach(element => {
+      countTo(parseInt(element.getAttribute('data-to')), 1500, element);
+    });
+  })
+  .catch(err => {
+    console.error("Error fetching guild count: ", err);
+  });
+
+function countTo(target, duration, element) {
+  const countFrom = 0;
+  const framesPerSecond = 60;
+  const totalFrames = framesPerSecond * (duration / 1000);
+  const increment = (target - countFrom) / totalFrames;
+
+  let currentCount = countFrom;
+
+  const counter = setInterval(function () {
+    currentCount += increment;
+    element.textContent = Math.round(currentCount);
+
+    if (currentCount >= target) {
+      element.textContent = target;
+      clearInterval(counter);
+    }
+  }, 1000 / framesPerSecond);
+}
+
+// ================
+// Reveal animation
+// ================
 
 window.addEventListener("scroll", reveal);
 
@@ -29,10 +94,9 @@ function reveal() {
         if (wasActivate == wasAnimate) {
           wasActivate = !wasActivate
           if (wasActivate) {
-            $('.value').countTo({
-              refreshInterval: 50,
-              speed: 1500
-          });
+            document.querySelectorAll('.value').forEach(element => {
+              countTo(parseInt(element.getAttribute('data-to')), 1500, element);
+            });
           }
         }
         wasAnimate = true
